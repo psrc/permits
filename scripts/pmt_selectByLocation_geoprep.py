9dca21236f86a87fa3e1caef81a1ac9557abd6f0
Christy Lam <cyyllam@gmail.com>
2017-08-24 10:54:08 -0700

    included county loop

--- scripts/pmt_selectByLocation_geoprep.py
+++ scripts/pmt_selectByLocation_geoprep.py
@@ -6,16 +6,16 @@ import os
 import arcpy
 
 # Dictionary containing joining features
-def prclDictWhole(x):
+def prclDictPath(x):
     return {
         '033': 'king\parcel_address_proj.shp',
-        '035': '',
+        '035': 'kitsap\parcels.shp',
         '053': '',
-        '061': '',
+        '061': 'snohomish\parcels.shp',
     }[x]
 
 prclRootDir = r'J:\Projects\Geocoding\17GEOCODING\Setup\3FinalProducts'
-counties = ['033'] #, '035', '053', '061'
+counties = ['033', '035', '061'] #, '053'
 
 workspace = r'C:\Users\CLam\Desktop\google_geo_shp'
 arcpy.env.workspace = workspace
@@ -27,49 +27,58 @@ fieldName1 = "X_COORD"
 fieldName2 = "Y_COORD"
 
 fc = arcpy.ListFeatureClasses()
-fcSubset = [f for f in fc if "033" in f]
 
-parcel = os.path.join(prclRootDir, prclDictWhole('033'))
-
-for fs in fcSubset:
-    # add XY coord fields to shapefile in workspace
-    arcpy.AddField_management(fs, fieldName1, "DOUBLE")
-    arcpy.AddField_management(fs, fieldName2, "DOUBLE")
-    print "Added new fields to " + fs 
-    # calculate centroid
-    arcpy.CalculateField_management(fs, fieldName1, "!SHAPE.CENTROID.X!", "PYTHON_9.3")
-    arcpy.CalculateField_management(fs, fieldName2, "!SHAPE.CENTROID.Y!", "PYTHON_9.3")
-    print "Updated XY coordinates to " + fs
-    
-    arcpy.MakeFeatureLayer_management(fs, "points_lyr")
-    totalRowCount = int(arcpy.GetCount_management("points_lyr")[0])
-    SelectedLayer = arcpy.SelectLayerByLocation_management("points_lyr", "intersect", parcel)
-    
-    matchcount = int(arcpy.GetCount_management(SelectedLayer)[0]) 
-    if matchcount == 0:
-        arcpy.MakeFeatureLayer_management(fs, "nomatch_points_lyr")
-        #export records of layer to 1inbox (so they can be geocoded)
-        outTableNoMatch = os.path.join(outWorkspace1, fs.strip(".shp") + ".dbf")
-        arcpy.CopyRows_management("nomatch_points_lyr", outTableNoMatch)
-        print "no features intersected with parcel layer from " + fs
+for cnty in counties:
+    fcSubset = [f for f in fc if cnty in f]
+#    fcSubset = [f for f in fc if "033" in f]
+    if len(fcSubset) == 0:
+        continue
     else:
-        #export attribute table where some joins took place to 0outbox (to update master files in permit database)
-        outTableSelected2 = os.path.join(outWorkspace2, fs.strip(".shp") + ".dbf")
-        arcpy.CopyRows_management(SelectedLayer, outTableSelected2)
-        print "Exported selected records of " + fs
-        if matchcount < totalRowCount: 
-            #invert selection and export remaining records to 1inbox (so they can be geocoded)
-            NonSelectedLayer = arcpy.SelectLayerByLocation_management("points_lyr", "intersect", parcel, invert_spatial_relationship = "invert")
-            outTableNonSelected = os.path.join(outWorkspace1, fs.strip(".shp") + ".dbf")
-            arcpy.CopyRows_management(NonSelectedLayer, outTableNonSelected)
-            print "Exported nonselected records of " + fs
+        parcel = os.path.join(prclRootDir, prclDictPath(cnty)) #prclDictPath('033')
         
-    arcpy.Delete_management(SelectedLayer)
-    arcpy.Delete_management("points_lyr") 
-    arcpy.Delete_management("nomatch_points_lyr") 
-    
+        for fs in fcSubset:
+            # check for XY coord fields
+            fieldNames = [field.name for field in arcpy.ListFields(fs)]
+            if ("X_COORD" not in fieldNames) & ("Y_COORD" not in fieldNames):
+                # add XY coord fields to shapefile in workspace
+                arcpy.AddField_management(fs, fieldName1, "DOUBLE")
+                arcpy.AddField_management(fs, fieldName2, "DOUBLE")
+                print "Added new fields to " + fs 
+                # calculate centroid
+                arcpy.CalculateField_management(fs, fieldName1, "!SHAPE.CENTROID.X!", "PYTHON_9.3")
+                arcpy.CalculateField_management(fs, fieldName2, "!SHAPE.CENTROID.Y!", "PYTHON_9.3")
+                print "Updated XY coordinates to " + fs
+            
+            arcpy.MakeFeatureLayer_management(fs, "points_lyr")
+            totalRowCount = int(arcpy.GetCount_management("points_lyr")[0])
+            SelectedLayer = arcpy.SelectLayerByLocation_management("points_lyr", "intersect", parcel)
+            
+            matchcount = int(arcpy.GetCount_management(SelectedLayer)[0]) 
+            if matchcount == 0:
+                arcpy.MakeFeatureLayer_management(fs, "nomatch_points_lyr")
+                #export records of layer to 1inbox (so they can be geocoded)
+                outTableNoMatch = os.path.join(outWorkspace1, fs.strip(".shp") + ".dbf")
+                arcpy.CopyRows_management("nomatch_points_lyr", outTableNoMatch)
+                print "no features intersected with parcel layer from " + fs
+            else:
+                #export attribute table where some joins took place to 0outbox (to update master files in permit database)
+                outTableSelected2 = os.path.join(outWorkspace2, fs.strip(".shp") + ".dbf")
+                arcpy.CopyRows_management(SelectedLayer, outTableSelected2)
+                print "Exported selected records of " + fs
+                if matchcount < totalRowCount: 
+                    #invert selection and export remaining records to 1inbox (so they can be geocoded)
+                    NonSelectedLayer = arcpy.SelectLayerByLocation_management("points_lyr", "intersect", parcel, invert_spatial_relationship = "invert")
+                    outTableNonSelected = os.path.join(outWorkspace1, fs.strip(".shp") + ".dbf")
+                    arcpy.CopyRows_management(NonSelectedLayer, outTableNonSelected)
+                    print "Exported nonselected records of " + fs
+                
+            arcpy.Delete_management(SelectedLayer)
+            arcpy.Delete_management("points_lyr") 
+            arcpy.Delete_management("nomatch_points_lyr") 
+        
+##-------------------------------------------------------------------------------------    
 #remove XY coordinates from .dbf in J:\Projects\Permits\16Permit\geocoding\prep\1inbox
-removeFields = [fieldName1, fieldName2]
+removeFields = [fieldName1, fieldName2, "lat", "long", "accurcy", "frmttd_"]
 
 # change workspace, remove XY coordinates from 1inbox tables
 arcpy.env.workspace = outWorkspace1
